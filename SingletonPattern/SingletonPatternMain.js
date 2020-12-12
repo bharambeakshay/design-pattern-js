83cc1b73bfc41506eaeb611519340433fda8422f
Akshay <bharambeakshay1997@gmail.com>
2020-12-12 15:09:09 +0530

    [Akshay] Refactor . file comments are added

--- SingletonPattern/SingletonPatternMain.js
+++ SingletonPattern/SingletonPatternMain.js
@@ -1,3 +1,12 @@
+/******************************************************************************************************
+* @purpose  :  Design a singleton design pattern for a booky persons performs various actions.
+* 	
+*  @Auther   : Akshay
+*  @File     : SingletonPatternMain.js 
+*  
+************************************************************************************************/
+
+
 const bookLibrary = require('./SingletonPatternBusinessLogic')
 
 let isBook = bookLibrary.book.getInstance();
