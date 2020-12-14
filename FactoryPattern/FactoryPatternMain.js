45a948c485d63c4cdc960d0509510d09831eecbd
Akshay <bharambeakshay1997@gmail.com>
2020-12-14 09:13:07 +0530

    [Akshay] Refactor. factory pattern's main method is changed

--- FactoryPattern/FactoryPatternMain.js
+++ FactoryPattern/FactoryPatternMain.js
@@ -1,8 +1,26 @@
-try {
-    let bookShelf = require('./FactoryPatternBusinessLogic')
-    new bookShelf.SelfHealingBooks().library();
-    new bookShelf.MotivationalBooks().library();
-    new bookShelf.RomanticBooks().library();
-} catch (error) {
-    console.log(error)
+const { BookShelf } = require('./FactoryPatternBusinessLogic');
+//const bookShelf = require('./FactoryPatternBusinessLogic')
+let bookShelf = new BookShelf();
+let tempBook;
+luck = Math.floor(Math.random() * 3);
+switch (luck) {
+    case 0:
+        tempBook = bookShelf.createShelf("Wear Yor Attitude", "selfhealing", "Jeff")
+        console.log("Self-Healing Books")
+        break
+    case 1:
+        tempBook = bookShelf.createShelf("You can Win", "motivational", "Shiv Khera")
+        console.log("Motivational Books")
+        break
+    case 2:
+        tempBook = bookShelf.createShelf("Love is in Air", "romantic", "Sudeep")
+        console.log("Romantic Books")
+        break;
+    default:
+        console.log("Please enter valid category");
+        break;
+
 }
+console.log(tempBook);
+console.log();
+
