b6e3505fcc610f58eed5c4dc65ac4908c98c2c48
Akshay <bharambeakshay1997@gmail.com>
2020-12-14 09:12:22 +0530

    [Akshay] Refactor. factory pattern's logic

--- FactoryPattern/FactoryPatternBusinessLogic.js
+++ FactoryPattern/FactoryPatternBusinessLogic.js
@@ -1,30 +1,59 @@
 
 //Books is the parent class and children classes are extended from it as shown below
 class Book {
-    constructor() { }
+    constructor(name, type, author) {
+        this.name = name;
+        this.type = type;
+        this.author = author;
+    }
+    printDetails() {
+        console.log("Book Name " + this.name);
+        console.log("Book type " + this.type);
+        console.log("Book author " + this.author);
 
-    library() {
-        throw new Error("Method 'library()' must be implemented.");
+
     }
 }
+
 class SelfHealingBooks extends Book {
-    library() {
-        console.log("-------------------------------------------");
-        console.log("Self Healing Books");
-    }
-}
-class MotivationalBooks extends Book {
-    library() {
-        console.log("--------------------------------------------");
-        console.log("Motivational Books");
+    constructor(name, type, author) {
+        super(name, type, author)
+        this.type ="selfhealing"
 
     }
 }
+
+
+
+
+class MotivationalBooks extends Book {
+    constructor(name, type, author) {
+        super(name, type, author)
+        this.type = "motivational"
+    }
+}
+
+
 
 class RomanticBooks extends Book {
-    library() {
-        console.log("----------------------------------------");
-        console.log("Romantic Books");
+    constructor(name, type, author) {
+        super(name, type, author)
+        this.type = "romantic"
     }
 }
-module.exports = { Book, MotivationalBooks, SelfHealingBooks, RomanticBooks }
+class BookShelf {
+    createShelf(name, type, author) {
+        switch (type) {
+            case "selfhealing":
+                return new SelfHealingBooks(name, type, author)
+            case "motivational":
+                return new MotivationalBooks(name, type, author)
+            case "romantic":
+                return new RomanticBooks(name, type, author)
+            default:
+                return null;
+        }
+    }
+}
+
+module.exports = { Book, MotivationalBooks, SelfHealingBooks, RomanticBooks, BookShelf }
